da1734e92bf74bf1b4d8c7a26715825cb18ae691
oleh32 <ukraine2342@gmail.com>
2022-06-14 00:31:13 +0300

    1

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 const path = require("path")
@@ -15,15 +14,6 @@ const buildPath = path.join(__dirname, '/build');
 app.use(express.static(buildPath));
 
 
-app.use(cors());
-app.options('*', cors());
-// app.use(function (req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,authorization');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     next();
-// });
 app.use(express.json());
 app.use(cookieParser());
 
