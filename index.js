6df5b3defb17f8b5966b5d99b8eda0df3d4b463b
oleh32 <ukraine2342@gmail.com>
2022-06-15 17:37:49 +0300

    1

--- index.js
+++ index.js
@@ -15,11 +15,10 @@ app.use(cors());
 app.options('*', cors());
 app.use(express.json());
 app.use(cookieParser());
-
-app.use(express.static(path.join(__dirname, "/build")));
+app.use(express.static(path.join(__dirname, "../../client/build")));
 
 app.use("/api", router)
-app.get("/*", (_, res) => { res.sendFile(path.join(__dirname, "/build/index.html")) });
+app.get("/*", (_, res) => { res.sendFile(path.join(__dirname, "../../client/build/index.html"))});
 app.use(errorHandler)
 
 
